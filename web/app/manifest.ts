aea36a92d2dc682b0bb1306e2c826de59bb213be
Your Name <your-email@example.com>
2026-05-27 10:51:06 +0530

    fix: version PWA app icons

--- web/app/manifest.ts
+++ web/app/manifest.ts
@@ -37,19 +37,19 @@ export default function manifest(): MetadataRoute.Manifest {
     dir: "ltr",
     icons: [
       {
-        src: "/icons/icon-192.png",
+        src: "/icons/icon-192-v2.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/icons/icon-512.png",
+        src: "/icons/icon-512-v2.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/icons/icon-512-maskable.png",
+        src: "/icons/icon-512-maskable-v2.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
